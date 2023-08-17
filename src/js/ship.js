116376506ede5839756b65dfb385da58eae6ce82
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-17 18:36:10 +0100

    Create isSunk method to pass test

--- src/js/ship.js
+++ src/js/ship.js
@@ -1,7 +1,14 @@
-const Ship = (length, timesHit = 0, isSunk = false) => ({
-  length,
-  timesHit,
-  isSunk,
-});
+const Ship = (length) => {
+  let timesHit = 0;
+
+  let isSunk = () => {};
+
+  const hit = () => {
+    timesHit += 1;
+    return timesHit;
+  };
+
+  return { length, timesHit, isSunk, hit };
+};
 
 export default Ship;
