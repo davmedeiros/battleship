25ea585ac87e0a81dcf30b965b0345563bbb3984
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-17 17:28:39 +0100

    Set Ship creation test

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -1,4 +1,5 @@
-test('When hit, returns ship remaining health', () => {
-  const ship = Ship();
-  expect(ship.hit()).toBe(2);
+import Ship from '../js/ship';
+
+test('When a ship object with length, number of hits and sunk boolean', () => {
+  expect(Ship(3, 0, false)).toEqual({ length: 3, timesHit: 0, isSunk: false });
 });
