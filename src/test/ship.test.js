e2714f9989546d48cac237566a2a8b90972bd8f8
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-17 17:30:26 +0100

    Change test to use default values

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -1,5 +1,5 @@
 import Ship from '../js/ship';
 
-test('When a ship object with length, number of hits and sunk boolean', () => {
-  expect(Ship(3, 0, false)).toEqual({ length: 3, timesHit: 0, isSunk: false });
+test('Creates a ship object with length, number of hits and sunk boolean', () => {
+  expect(Ship(3)).toEqual({ length: 3, timesHit: 0, isSunk: false });
 });
