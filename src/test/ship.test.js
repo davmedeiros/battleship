a5a8b6eddcc0c2947165623862e138d6a7a08db9
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-17 18:33:26 +0100

    Make object creation test more standard
    Here I am only checking if the object gets created with the properties
    and methods.

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -1,5 +1,16 @@
 import Ship from '../js/ship';
 
-test('Creates a ship object with length, number of hits and sunk boolean', () => {
-  expect(Ship(3)).toEqual({ length: 3, timesHit: 0, isSunk: false });
+test('Creates a ship object with length, timesHit, isSunk() and hit()', () => {
+  const expected = {
+    length: 3,
+    timesHit: 0,
+    isSunk: expect.any(Function),
+    hit: expect.any(Function),
+  };
+  const actual = Ship(3);
+  expect(actual).toEqual(expected);
+});
+
+test('When ship is hit, return the times it was hit', () => {
+  expect(Ship(3).hit()).toBe(1);
 });
