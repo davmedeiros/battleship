22db0896c93597bbab512cafb2e528738e19da4e
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-18 10:12:29 +0100

    Write unsuccessful placeShip() test

--- src/test/game-board.test.js
+++ src/test/game-board.test.js
@@ -11,8 +11,15 @@ test('GameBoard object is created with attributes and methods', () => {
   expect(actual).toEqual(expected);
 });
 
-test("Returns a ship if it's able to place it", () => {
+test("Returns something if it's able to place the ship", () => {
   const gameBoard = GameBoard();
   const actual = gameBoard.placeShip(3, 3, 2);
   expect(actual).not.toBeUndefined();
+});
+
+test("Returns undefined if the ship can't be placed", () => {
+  const gameBoard = GameBoard();
+  gameBoard.placeShip(3, 3, 2);
+  const place2 = gameBoard.placeShip(3, 3, 2);
+  expect(place2).toBeUndefined();
 });
