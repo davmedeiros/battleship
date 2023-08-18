a0cd5d2f60ce81adceada76e9acc334f59570fd2
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-18 08:10:31 +0100

    Write GameBoard creation tests

---
+++ src/test/game-board.test.js
@@ -0,0 +1,10 @@
+test('GameBoard object is created with attributes and methods', () => {
+  const expected = {
+    placeShip: expect.any(Function),
+    receiveAttack: expect.any(Function),
+    missedAttacks: 0,
+    hasAllSunk: false,
+  };
+  const actual = GameBoard();
+  expect(gameBoard).toEqual(actual);
+});
