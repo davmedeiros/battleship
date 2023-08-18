beb0091419b9278c78716c3246a5e78c14ef8c46
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-18 14:12:32 +0100

    Add player creation test

---
+++ src/test/player.test.js
@@ -0,0 +1,7 @@
+import Player from '../js/player';
+
+test("Creates a player with a name and if it's AI or not", () => {
+  const expected = { name: 'John Doe', isAI: false };
+  const player = Player('John Doe', false);
+  expect(player).toBe(expected);
+});
