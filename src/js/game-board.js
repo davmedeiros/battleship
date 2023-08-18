3168d3d9f1efb7605eb4e005b7f14808b69cae77
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-18 08:29:13 +0100

    Implement GameBoard factory

--- src/js/game-board.js
+++ src/js/game-board.js
@@ -1,5 +1,9 @@
 const GameBoard = () => {
   const placeShip = () => {};
+  const receiveAttack = () => {};
+  const hasAllSunk = () => {};
+
+  return { placeShip, receiveAttack, hasAllSunk };
 };
 
 export default GameBoard;
