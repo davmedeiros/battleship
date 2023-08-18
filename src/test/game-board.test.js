8d06e8d799af1db7b0617e54f945db335e3d949b
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-18 08:24:09 +0100

    Change test to not expect missedAttacks
    These will be handled internally

--- src/test/game-board.test.js
+++ src/test/game-board.test.js
@@ -4,8 +4,7 @@ test('GameBoard object is created with attributes and methods', () => {
   const expected = {
     placeShip: expect.any(Function),
     receiveAttack: expect.any(Function),
-    missedAttacks: 0,
-    hasAllSunk: false,
+    hasAllSunk: expect.any(Function),
   };
   const actual = GameBoard();
   expect(actual).toEqual(expected);
