861f07afe09f8de380f825ffa8cded08f32e50a2
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-18 14:18:56 +0100

    Change test to expect attack()

--- src/test/player.test.js
+++ src/test/player.test.js
@@ -1,7 +1,11 @@
 import Player from '../js/player';
 
 test("Creates a player with a name and if it's AI or not", () => {
-  const expected = { name: 'John Doe', isAI: false };
+  const expected = {
+    name: 'John Doe',
+    isAI: false,
+    attack: expect.any(Function),
+  };
   const player = Player('John Doe');
   expect(player).toEqual(expected);
 });
