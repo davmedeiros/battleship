4d0b3d9a12d6d1f5bc7aecc0b558d2c25c2439af
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-19 09:46:32 +0100

    Also create enemy board view

--- src/js/dom.js
+++ src/js/dom.js
@@ -4,14 +4,20 @@ const game = Game('Admiral');
 const boardViews = document.querySelectorAll('.board');
 
 const createBoards = () => {
-  const columns = game.player.getGameBoard().getBoard();
-  console.table(columns);
-  columns.forEach((rows) => {
-    rows.forEach((spot) => {
-      const spotView = document.createElement('div');
-      spotView.classList.add('spot');
-      boardViews[0].appendChild(spotView);
+  const playerBoard = game.player.getGameBoard().getBoard();
+  const enemyBoard = game.enemy.getGameBoard().getBoard();
+  const boards = [playerBoard, enemyBoard];
+  let index = 0;
+
+  boardViews.forEach((boardView) => {
+    boards[index].forEach((rows) => {
+      rows.forEach((spot) => {
+        const spotView = document.createElement('div');
+        spotView.classList.add('spot');
+        boardView.appendChild(spotView);
+      });
     });
+    index += 1;
   });
 };
 
