10cacacfb84825d354924cdcb4d3a8663f2c6b04
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-19 07:58:46 +0100

    Write test to see if play() affects ships

--- src/test/game.test.js
+++ src/test/game.test.js
@@ -14,3 +14,11 @@ test('Returns something when it plays successfully', () => {
   const game = Game('John Doe');
   expect(game.play(3, 2)).not.toBeUndefined();
 });
+
+test("play() affects the player's ships", () => {
+  const game = Game('John Doe');
+  const { enemy } = game;
+  const ship = enemy.getBoard().placeShip(3, 3, 2);
+  game.play(3, 2);
+  expect(ship.hit()).toBe(2);
+});
