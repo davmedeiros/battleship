660d419c73f7056fe704b25c47487abbde6ad2af
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-19 08:51:53 +0100

    Delete play(), the modules are simple enough
    The play() function was meant to act as an abstraction between the ui
    and the modules themselves. As the current application is built it seems
    redundant as everything is simple to call directly.

--- src/test/game.test.js
+++ src/test/game.test.js
@@ -4,22 +4,7 @@ test('Creates a game', () => {
   const expected = {
     player: expect.any(Object),
     enemy: expect.any(Object),
-    play: expect.any(Function),
   };
   const game = Game('John Doe');
   expect(game).toEqual(expected);
-});
-
-test('Returns something when it plays successfully', () => {
-  const game = Game('John Doe');
-  game.enemy.getBoard().placeShip(3, 3, 2);
-  expect(game.play(3, 2)).not.toBeUndefined();
-});
-
-test("play() affects the player's ships", () => {
-  const game = Game('John Doe');
-  const { enemy } = game;
-  const ship = enemy.getBoard().placeShip(3, 3, 2);
-  game.play(3, 2);
-  expect(ship.hit()).toBe(2);
 });
