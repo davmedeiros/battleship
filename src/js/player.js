eba7d6ea6a8dd2d1299d02c8024556c76deface2
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-20 12:04:19 +0100

    Improve AI, now the AI doesn't try to do diagonals
    Important: Sometimes the AI gets stuck and blows up the browser

--- src/js/player.js
+++ src/js/player.js
@@ -3,10 +3,14 @@ import GameBoard from './game-board';
 const Player = (name, isAI = false) => {
   const board = GameBoard();
   // AI variables
+  let originalHitCoordinatesY = -1;
+  let originalHitCoordinatesX = -1;
   let lastHitCoordinatesY = -1;
   let lastHitCoordinatesX = -1;
   let isHittingVertical = false;
   let isHittingHorizontal = false;
+  let shotsLandedOnCurrent = 0;
+  let hasHitLastTime = false;
 
   const aiAttack = (enemyBoard) => {
     let result;
@@ -21,19 +25,56 @@ const Player = (name, isAI = false) => {
         target = enemyBoard.getBoard()[coordinatesY][coordinatesX];
       } else {
         if (isHittingHorizontal) {
-          coordinatesX =
-            lastHitCoordinatesX + (Math.floor(Math.random() * 2) ? 1 : -1);
+          if (!hasHitLastTime && (!target || target === 'shot')) {
+            coordinatesX =
+              originalHitCoordinatesX +
+              (Math.floor(Math.random() * 2) ? 1 : -1);
+          } else {
+            coordinatesX =
+              lastHitCoordinatesX + (Math.floor(Math.random() * 2) ? 1 : -1);
+          }
+
           coordinatesY = lastHitCoordinatesY;
         } else if (isHittingVertical) {
-          coordinatesY =
-            lastHitCoordinatesY + (Math.floor(Math.random() * 2) ? 1 : -1);
+          if (!hasHitLastTime && (!target || target === 'shot')) {
+            coordinatesY =
+              originalHitCoordinatesY +
+              (Math.floor(Math.random() * 2) ? 1 : -1);
+          } else {
+            coordinatesY =
+              lastHitCoordinatesY + (Math.floor(Math.random() * 2) ? 1 : -1);
+          }
+
           coordinatesX = lastHitCoordinatesX;
         } else {
-          coordinatesY =
-            lastHitCoordinatesY + Math.floor(Math.random() * (2 - -1) + -1);
-          coordinatesX =
-            lastHitCoordinatesX + Math.floor(Math.random() * (2 - -1) + -1);
+          let offset = Math.floor(Math.random() * (2 - -1) + -1);
+          coordinatesY = lastHitCoordinatesY + offset;
+
+          if (offset) {
+            offset = 0;
+          } else {
+            offset = Math.floor(Math.random() * (2 - -1) + -1);
+          }
+
+          coordinatesX = lastHitCoordinatesX + offset;
         }
+
+        if (coordinatesY > 9) {
+          coordinatesY = 9;
+        }
+
+        if (coordinatesY < 0) {
+          coordinatesY = 0;
+        }
+
+        if (coordinatesX > 9) {
+          coordinatesX = 9;
+        }
+
+        if (coordinatesX < 0) {
+          coordinatesX = 0;
+        }
+
         target = enemyBoard.getBoard()[coordinatesY][coordinatesX];
       }
     }
@@ -41,11 +82,20 @@ const Player = (name, isAI = false) => {
     if (!enemyBoard.hasAllSunk()) {
       result = enemyBoard.receiveAttack(coordinatesY, coordinatesX);
 
-      if (result) {
+      if (result && result !== 'shot') {
+        if (shotsLandedOnCurrent === 0) {
+          originalHitCoordinatesY = coordinatesY;
+          originalHitCoordinatesX = coordinatesX;
+        }
+
+        hasHitLastTime = true;
+        shotsLandedOnCurrent += 1;
+
         if (result.isSunk()) {
           [lastHitCoordinatesY, lastHitCoordinatesX] = [-1, -1];
           isHittingVertical = false;
           isHittingHorizontal = false;
+          shotsLandedOnCurrent = 0;
         } else {
           if (lastHitCoordinatesY === coordinatesY) {
             isHittingHorizontal = true;
@@ -58,6 +108,8 @@ const Player = (name, isAI = false) => {
             coordinatesX,
           ];
         }
+      } else {
+        hasHitLastTime = false;
       }
     }
 
