fbccacbf30ffe2e74d2bb168eb29972976c7ce0e
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-20 09:28:39 +0100

    Implement proper AI, moves aren't just random now

--- src/js/player.js
+++ src/js/player.js
@@ -2,6 +2,11 @@ import GameBoard from './game-board';
 
 const Player = (name, isAI = false) => {
   const board = GameBoard();
+  // AI variables
+  let lastHitCoordinatesY = -1;
+  let lastHitCoordinatesX = -1;
+  let isHittingVertical = false;
+  let isHittingHorizontal = false;
 
   const aiAttack = (enemyBoard) => {
     let result;
@@ -10,13 +15,50 @@ const Player = (name, isAI = false) => {
     let target = 'shot';
 
     while (target === 'shot' && !enemyBoard.hasAllSunk()) {
-      coordinatesY = Math.floor(Math.random() * 10);
-      coordinatesX = Math.floor(Math.random() * 10);
-      target = enemyBoard.getBoard()[coordinatesY][coordinatesX];
+      if (lastHitCoordinatesY === -1 && lastHitCoordinatesX === -1) {
+        coordinatesY = Math.floor(Math.random() * 10);
+        coordinatesX = Math.floor(Math.random() * 10);
+        target = enemyBoard.getBoard()[coordinatesY][coordinatesX];
+      } else {
+        if (isHittingHorizontal) {
+          coordinatesX =
+            lastHitCoordinatesX + (Math.floor(Math.random() * 2) ? 1 : -1);
+          coordinatesY = lastHitCoordinatesY;
+        } else if (isHittingVertical) {
+          coordinatesY =
+            lastHitCoordinatesY + (Math.floor(Math.random() * 2) ? 1 : -1);
+          coordinatesX = lastHitCoordinatesX;
+        } else {
+          coordinatesY =
+            lastHitCoordinatesY + Math.floor(Math.random() * (2 - -1) + -1);
+          coordinatesX =
+            lastHitCoordinatesX + Math.floor(Math.random() * (2 - -1) + -1);
+        }
+        target = enemyBoard.getBoard()[coordinatesY][coordinatesX];
+      }
     }
 
     if (!enemyBoard.hasAllSunk()) {
       result = enemyBoard.receiveAttack(coordinatesY, coordinatesX);
+
+      if (result) {
+        if (result.isSunk()) {
+          [lastHitCoordinatesY, lastHitCoordinatesX] = [-1, -1];
+          isHittingVertical = false;
+          isHittingHorizontal = false;
+        } else {
+          if (lastHitCoordinatesY === coordinatesY) {
+            isHittingHorizontal = true;
+          } else if (lastHitCoordinatesX === coordinatesX) {
+            isHittingVertical = true;
+          }
+
+          [lastHitCoordinatesY, lastHitCoordinatesX] = [
+            coordinatesY,
+            coordinatesX,
+          ];
+        }
+      }
     }
 
     return result;
